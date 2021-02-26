2373ef2057844f3c8341c8e4971fcb0febd204dc
EvgeniyOne <buturlakevgeniy@gmail.com>
2021-02-26 12:43:52 +0300

    add GUI and custom property

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,28 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    setupUi(this);
+    pushButton->setProperty("pagematches",true);
+
 }
 
-MainWindow::~MainWindow()
+
+void MainWindow::on_tabWidget_currentChanged(int index)
 {
-    delete ui;
+    pushButton->setProperty("pagematches", false);
+    pushButton_2->setProperty("pagematches", false);
+    pushButton_3->setProperty("pagematches", false);
+    // Set one of the buttons to true
+    if (index == 0)
+        pushButton->setProperty("pagematches", true);
+    else if (index == 1)
+        pushButton_2->setProperty("pagematches", true);
+    else
+        pushButton_3->setProperty("pagematches", true);
+
+    pushButton->style()->polish(pushButton);
+    pushButton_2->style()->polish(pushButton_2);
+    pushButton_3->style()->polish(pushButton_3);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStyle>
+#include <ui_mainwindow.h>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow,public Ui::MainWindow
 {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+private slots:
+    void on_tabWidget_currentChanged(int index);
 
 private:
-    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
